b87e2bba3874c8ea82e8ba81d0ba78e8409454d3
Gokul <gokul.g@external.nexigroup.com>
2024-02-08 16:32:48 +0530

    Initial Commit

---
+++ Jam/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Jam
+//
+//  Created by Gokul on 08/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jam/JamApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamApp.swift
+//  Jam
+//
+//  Created by Gokul on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct JamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
